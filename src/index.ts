2fa419c03b15a351cdf29c60044aa30752cc6a60
Essam Sayed <essamhewala84@gmail.com>
2023-01-20 15:55:01 +0200

    Add Jasmine

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("hello");
+import express , {Application}  from "express"
+const port = 3200;
+const app : Application = express();
+app.get("/",(req,res)=>{
+    res.send("hellooo");
+})
+app.listen(port,()=>{
+    console.log(`Server Running On Port : ${port}`);
+})
+export default app;
---
+++ src/test/indexSpec.ts
@@ -0,0 +1,10 @@
+import Supertest from 'supertest'
+import app from '../index'
+const resquest = Supertest(app);
+describe("test basic server",()=>{
+    it("get the / from the server", async()=>{
+        const res = await resquest.get('/');
+        expect(res.status).toBe(200);
+
+    })
+})
