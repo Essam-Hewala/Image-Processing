a79995fe6e3d00337e0dcd928050583fac128e83
Essam Sayed <essamhewala84@gmail.com>
2023-01-21 21:52:14 +0200

    Fix Bug

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,9 @@ const port = 3000;
 const app: Application = express();
 // main page
 app.get("/", (req, res) => {
-  res.send("<h1>Hello From Main Page You Have To Enter Data Like The Below Example To See Results</h1><h2>img?name='Image Name'&width='width number'&height='Height Number'</h2>");
-
+  res.send(
+    "<h1>Hello From Main Page You Have To Enter Data Like The Below Example To See Results</h1><h2>img?name='Image Name'&width='width number'&height='Height Number'</h2>"
+  );
 });
 app.use(express.static(cachedimgdir));
 // resizing page
@@ -21,12 +22,16 @@ app.get("/img", (req, res) => {
     // check if image already Resized Or Not
     fs.existsSync(
       `${cachedimgdir}/${resize.name}_${resize.width}_${resize.height}.jpg`
-    )) {
-      res.status(200)
-    res.send("Image Have Been Resized before This Is The Cached One :" +`<img src="${resize.name}_${resize.width}_${resize.height}.jpg">`);
+    )
+  ) {
+    res.status(200);
+    res.send(
+      "Image Have Been Resized before This Is The Cached One :" +
+        `<img src="${resize.name}_${resize.width}_${resize.height}.jpg">`
+    );
   } else {
     resizes(resize.width, resize.height, resize.name).then((next) => {
-      res.send( "Resized New Image :" + `<img src="${next}">`);
+      res.send("Resized New Image :" + `<img src="${next}">`);
     });
   }
 });
--- src/test/operationSpec.ts
+++ src/test/operationSpec.ts
@@ -1,13 +1,13 @@
 import Supertest from "supertest";
-import { cachedimgdir} from "../router/router";
+import { cachedimgdir } from "../router/router";
 import fs from "fs";
 import resizes from "../router/operatins";
 describe("Testing Functions Resizing", () => {
-    it('Test image  720*720', async () => {
-      if (fs.existsSync(`${cachedimgdir}/encenadaport_100_100.jpg`)) {
-        fs.unlinkSync(`${cachedimgdir}/encenadaport_100_100.jpg`);
-      }
-      const resized = await resizes(720, 720, 'encenadaport');
-      expect(resized).toBe('encenadaport_720_720.jpg');
-    });
+  it("Test image  720*720", async () => {
+    if (fs.existsSync(`${cachedimgdir}/task_720_720.jpg`)) {
+      fs.unlinkSync(`${cachedimgdir}/task_720_720.jpg`);
+    }
+    const resized = await resizes(720, 720, "task");
+    expect(resized).toBe("task_720_720.jpg");
+  });
 });
